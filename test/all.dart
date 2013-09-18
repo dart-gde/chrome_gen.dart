41a0e290a906bd1082eb00d1bb40f3c748bb6da3
devoncarew@gmail.com <devoncarew@gmail.com>
2013-09-17 23:28:08 -0700

    removed unused files

--- test/all.dart
+++ test/all.dart
@@ -4,10 +4,8 @@
 
 library all_test;
 
-import 'parser_json_test.dart' as parser_json_test;
 import 'model_json_test.dart' as model_json_test;
 import 'src_gen_test.dart' as src_gen_test;
-import 'translation_test.dart' as translation_test;
 import 'utils_test.dart' as utils_test;
 import 'web_idl_files_test.dart' as test_web_idl_files;
 import 'web_idl_test.dart' as test_web_idl;
@@ -27,10 +25,8 @@ import 'web_idl_sequences_test.dart' as test_web_idl_sequences;
 import 'web_idl_typedefs_test.dart' as test_web_idl_typedefs;
 
 main() {
-  parser_json_test.main();
   model_json_test.main();
   src_gen_test.main();
-  translation_test.main();
   utils_test.main();
   test_web_idl_files.defineTests(includeWebIdl: false);
   test_web_idl.main();
