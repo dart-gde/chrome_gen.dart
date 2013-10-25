d6a974456f39242fb2dc0be20115f19bb3e0f87a
Adam Singer <financeCoding@gmail.com>
2013-10-25 14:28:37 -0700

    Adding more doc string test

--- test/chrome_idl_test.dart
+++ test/chrome_idl_test.dart
@@ -5,7 +5,7 @@ import 'package:unittest/unittest.dart';
 import '../tool/chrome_idl_parser.dart';
 
 void chromeIDLParserDocStringTests() {
-  test('Parse comment with **', () {
+  test('comment with **', () {
     ChromeIDLParser chromeIDLParser = new ChromeIDLParser();
     var doc = chromeIDLParser.docString.parse("/** Some comment */");
     expect(doc.runtimeType.toString(), equals("List"));
@@ -13,7 +13,29 @@ void chromeIDLParserDocStringTests() {
     expect(doc[0], equals(" Some comment "));
   });
 
-  test('Parse comment with *', () {
+  test('comment with ** multiline', () {
+    ChromeIDLParser chromeIDLParser = new ChromeIDLParser();
+    var doc = chromeIDLParser.docString.parse("""
+/**
+ * Some comment
+ *
+ * Some comment information.
+ * Some more comment information.
+ *
+ */""");
+    expect(doc.runtimeType.toString(), equals("List"));
+    expect(doc.length, equals(1));
+    expect(doc[0], equals(
+        '\n'
+        ' * Some comment\n'
+        ' *\n'
+        ' * Some comment information.\n'
+        ' * Some more comment information.\n'
+        ' *\n'
+        ' '));
+  });
+
+  test('comment with *', () {
     ChromeIDLParser chromeIDLParser = new ChromeIDLParser();
     var doc = chromeIDLParser.docString.parse("/* Some comment */");
     expect(doc.runtimeType.toString(), equals("List"));
@@ -21,15 +43,57 @@ void chromeIDLParserDocStringTests() {
     expect(doc[0], equals(" Some comment "));
   });
 
-  test('Parse comment with //', () {
+  test('comment with * multiline', () {
+    ChromeIDLParser chromeIDLParser = new ChromeIDLParser();
+    var doc = chromeIDLParser.docString.parse("""
+/*
+ * Some comment
+ *
+ * Some comment information.
+ * Some more comment information.
+ *
+ */""");
+    expect(doc.runtimeType.toString(), equals("List"));
+    expect(doc.length, equals(1));
+    expect(doc[0], equals(
+        '\n'
+        ' * Some comment\n'
+        ' *\n'
+        ' * Some comment information.\n'
+        ' * Some more comment information.\n'
+        ' *\n'
+        ' '));
+  });
+
+  test('comment with //', () {
     ChromeIDLParser chromeIDLParser = new ChromeIDLParser();
     var doc = chromeIDLParser.docString.parse("// Some comment\n");
     expect(doc.runtimeType.toString(), equals("List"));
     expect(doc.length, equals(1));
     expect(doc[0], equals(" Some comment"));
   });
+
+  test('comment with // multiline', () {
+    ChromeIDLParser chromeIDLParser = new ChromeIDLParser();
+    var doc = chromeIDLParser.docString.parse("""
+//
+// Some comment
+//
+// Some comment information.
+// Some more comment information.
+//
+//""");
+    expect(doc.runtimeType.toString(), equals("List"));
+    expect(doc.length, equals(6));
+    expect(doc[0], equals(''));
+    expect(doc[1], equals(' Some comment'));
+    expect(doc[2], equals(''));
+    expect(doc[3], equals(' Some comment information.'));
+    expect(doc[4], equals(' Some more comment information.'));
+    expect(doc[5], equals(''));
+  });
 }
 
 void main() {
-  group('ChromeIDLParser.docString', chromeIDLParserDocStringTests);
+  group('ChromeIDLParser.docString.parse', chromeIDLParserDocStringTests);
 }
