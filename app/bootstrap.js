bf11952396043d98631d4d9b09bc9f6c046b8b70
devoncarew@gmail.com <devoncarew@gmail.com>
2013-11-24 10:56:28 -0800

    tweak to the bootstrap file

--- app/bootstrap.js
+++
@@ -1,6 +0,0 @@
-
-if (navigator.userAgent.indexOf('(Dart)') === -1) {
-  var script = document.createElement('script');
-  script.src = 'demo.dart.precompiled.js';
-  document.body.appendChild(script);
-}
